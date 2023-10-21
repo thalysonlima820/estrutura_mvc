066d06aa107c7c9e6204ef37fffa4853c20af520
thalysonlima820 <thalysonlima820@gmail.com>
2023-10-21 17:15:05 -0300

    estrutura mvc

--- App/Models/Produto.php
+++ App/Models/Produto.php
@@ -6,6 +6,18 @@ use MF\Model\Model;
 
 class Produto extends Model {
 
+	private $id;
+    private $nome;
+
+    public function __get($atributo){
+        return $this->$atributo;
+    }
+
+    public function __set($atributo, $value){
+        $this->$atributo = $value;
+    }
+
+
 	public function getProdutos() {
 		
 		$query = "select id, descricao, preco from tb_produtos";
